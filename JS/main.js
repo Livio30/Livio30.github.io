2039a489675b2fdd73cc37ec44e6cda26e5aaef5
Livio30 <liviofranm@gmail.com>
2022-05-24 23:43:34 +0530

    Final Website (Not Mobile Responsive)

--- JS/main.js
+++ JS/main.js
@@ -14,6 +14,28 @@ $(document).ready(function(){
 
         return false;
     })
-    console.log("Hello");
 
+    $('.project-area .button-group #btn1').trigger('click');
+
+    $('.project-area .grid .project-popup-link').magnificPopup({
+        type: 'image',
+        gallery:{enabled:true}
+      });
+
+      let nav_offset_top = $('.header-area').height() + 50;
+
+      function navbarFixed() {
+          if ($('.header-area').length) {
+              $(window).scroll(function () {
+                  let scroll = $(window).scrollTop();
+                  if (scroll >= nav_offset_top) {
+                      $('.header-area .main-menu').addClass('navbar_fixed');
+                  } else {
+                      $('.header-area .main-menu').removeClass('navbar_fixed');
+                  }
+              })
+          }
+      }
+  
+      navbarFixed();
 });
