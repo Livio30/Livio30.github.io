e067b34a20488e4a6e170677e99c4df5363db22a
Livio30 <liviofranm@gmail.com>
2022-05-25 22:41:00 +0530

    Done!!!

--- JS/main.js
+++ JS/main.js
@@ -24,18 +24,34 @@ $(document).ready(function(){
 
       let nav_offset_top = $('.header-area').height() + 50;
 
-      function navbarFixed() {
-          if ($('.header-area').length) {
-              $(window).scroll(function () {
-                  let scroll = $(window).scrollTop();
-                  if (scroll >= nav_offset_top) {
-                      $('.header-area .main-menu').addClass('navbar_fixed');
-                  } else {
-                      $('.header-area .main-menu').removeClass('navbar_fixed');
-                  }
-              })
-          }
-      }
-  
-      navbarFixed();
+    function navbarFixed() {
+        if ($('.header-area').length) {
+            console.log(nav_offset_top);
+            $(window).scroll(function () {
+                let scroll = $(window).scrollTop();
+                if (scroll >= nav_offset_top) {
+                    $('.header-area .main-menu').addClass('navbar_fixed');
+                } else {
+                    $('.header-area .main-menu').removeClass('navbar_fixed');
+                }
+            })
+        }
+    }
+
+    navbarFixed();
+    let screen_width = $(window).width();
+    $(window).resize(function() {
+        screen_width = $(window).width();
+        // console.log('window was resized');
+        console.log(screen_width);
+        if(screen_width < 670)
+        {
+            $('.site-main .project-area .button-group').addClass('custom-flex');
+            // $('.site-main .service-area .container').addClass('justify-content-center');
+            $('.site-main .project-area .button-group button').addClass('p-0');
+        }else{
+            $('.site-main .project-area .button-group').removeClass('custom-flex');
+            $('.site-main .project-area .button-group button').removeClass('p-0');
+        }
+    });
 });
